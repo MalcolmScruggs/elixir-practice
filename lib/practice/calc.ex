666de06c0fe84656f095129ddc1c28c88a4c53cd
MalcolmScruggs <mookie.dev@gmail.com>
2018-09-18 12:58:09 -0400

    implement evaluating expressions

--- lib/practice/calc.ex
+++ lib/practice/calc.ex
@@ -7,18 +7,22 @@ defmodule Practice.Calc do
   def calc(expr) do
     # This should handle +,-,*,/ with order of operations,
     # but doesn't need to handle parens.
-    expr
-    |> String.split(~r/\s+/)
-    |> hd
-    |> parse_float
-    |> :math.sqrt()
-
-    # Hint:
-    # expr
-    # |> split
-    # |> tag_tokens  (e.g. [+, 1] => [{:op, "+"}, {:num, 1.0}]
-    # |> convert to postfix
-    # |> reverse to prefix
-    # |> evaluate as a stack calculator using pattern matching
+    case isOnlyNumAndOperators(expr) do
+      true -> evaluate(expr)
+      false -> "Invalid expression. Expression must only contain numbers and following operations: +, -, *, /"
+    end
   end
+
+  def evaluate(expr) do
+    x = Code.eval_string(expr)
+    elem(x, 0)
+  end
+
+  def isOnlyNumAndOperators(str) do
+    cond do
+         String.match?(str, ~r/^[0-9\+\-\/\*\(\)\ ]+$/) -> true
+         true -> false
+    end
+  end
+
 end
